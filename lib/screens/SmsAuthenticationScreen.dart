f06d0cd2c8451de2e394ccc17370b9bf8b48b7df
Matheus Godoi <matheus.godoi@monetizze.com.br>
2020-09-19 12:10:06 -0300

    Alterando tela após enviar a SMS

--- lib/screens/SmsAuthenticationScreen.dart
+++ lib/screens/SmsAuthenticationScreen.dart
@@ -13,68 +13,92 @@ class SmsAuthenticationScreen extends StatefulWidget {
 class _SmsAuthenticationScreenState extends State<SmsAuthenticationScreen> {
 
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  BuildContext _context;
   PhoneNumber _internationalPhoneNumberInput = PhoneNumber(isoCode: 'BR');
-  String _fullNumber = '';
   final TextEditingController _internationalPhoneNumberInputController = new MaskedTextController(mask: '(00) 00000-0000');
-
+  String _fullNumber = '';
+  bool _smsCodeSent = false;
 
   @override
   Widget build(BuildContext context) {
-    _context = context;
     return Scaffold(
       body: SafeArea(
-        child: FutureBuilder(
-          future: _initialization,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if(snapshot.connectionState == ConnectionState.done) {
-              return Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: InternationalPhoneNumberInput(
-                          onInputChanged: (value) => _fullNumber = value.toString(),
-                          ignoreBlank: false,
-                          autoValidate: false,
-                          selectorTextStyle: TextStyle(color: Colors.black),
-                          textFieldController: _internationalPhoneNumberInputController,
-                          inputBorder: OutlineInputBorder(),
-                          initialValue: _internationalPhoneNumberInput,
-                        ),
-                      ),
-                      CupertinoButton.filled(child: Text('Enviar código'), onPressed: () => _sendCodeHandler())
-                    ],
-                  ),
-                ),
-              );
-            }
-
-            return CircularProgressIndicator();
-          },
-        ),
+        child: !_smsCodeSent
+        ? _getSendSmsScreen()
+        : _getInsertSmsCodeScreen(),
       ),
     );
   }
 
-  Future<void> _sendCodeHandler() async {
-    print(_fullNumber);
-    return false;
+  ///Tela de envio de sms
+  Widget _getSendSmsScreen() {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if(snapshot.connectionState == ConnectionState.done) {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: InternationalPhoneNumberInput(
+                      onInputChanged: (value) => _fullNumber = value.toString(),
+                      ignoreBlank: false,
+                      autoValidate: false,
+                      selectorTextStyle: TextStyle(color: Colors.black),
+                      textFieldController: _internationalPhoneNumberInputController,
+                      inputBorder: OutlineInputBorder(),
+                      initialValue: _internationalPhoneNumberInput,
+                    ),
+                  ),
+                  CupertinoButton.filled(child: Text('Enviar código'), onPressed: () => _sendCodeToNumber())
+                ],
+              ),
+            ),
+          );
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  Widget _getInsertSmsCodeScreen() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('O código foi enviado para o número:'),
+          Text(_fullNumber, style: TextStyle(
+           fontSize: 18.0,
+           fontWeight: FontWeight.bold,
+           height: 2,
+          ))
+        ],
+      ),
+    );
+  }
+
+  Future<void> _sendCodeToNumber() async {
 
     await FirebaseAuth.instance.verifyPhoneNumber(
-      phoneNumber: '+5531993473620',
-      verificationCompleted: (PhoneAuthCredential credential) {
-        print("Verificacao completa");
+      phoneNumber: _fullNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        await FirebaseAuth.instance.signInWithCredential(credential);
       },
       verificationFailed: (FirebaseAuthException e) {
-        print("Verificação falha");
-        print(e);
+        String _errorMessage = 'Não foi possível enviar o código';
+        if (e.code == 'invalid-phone-number') {
+          _errorMessage = 'O número de telefone é inválido';
+        }
+        _showErrorMessage(_errorMessage);
       },
       codeSent: (String verificationId, int resendToken) {
         print("Código enviado");
+        setState(() {
+          _smsCodeSent = true;
+        });
       },
       codeAutoRetrievalTimeout: (String verificationId) {},
     );
@@ -84,11 +108,11 @@ class _SmsAuthenticationScreenState extends State<SmsAuthenticationScreen> {
 
   }
 
-  Future<void> _showErrorMessage(String errorMessage) {
+  Future<void> _showErrorMessage(String errorMessage, [String title]) {
     showDialog(
       context: context,
       builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text('Desculpe, encontramos um erro.'),
+        title: Text(title ?? 'Desculpe, encontramos um erro.'),
         content: Text(errorMessage),
         actions: [
           CupertinoDialogAction(
